81f8efb3ee7fb20fc248384a5da88e0a74f88dea
EsratJahanJui <esratjahanjui327@gmail.com>
2026-07-16 14:06:57 +0600

    add functionality to all, interview & reject section

--- index.js
+++ index.js
@@ -2,13 +2,18 @@
  const tabActive = [ "active"];
  const tabInactive = ["inactive"];
 
+ const allContainer = document.getElementById("all-container");
+ const interviewContainer = document.getElementById("interview-container");
+ const rejectContainer = document.getElementById("reject-container");
+//  console.log(allContainer, interviewContainer, rejectContainer);
  function switchTab(tab){
-    console.log(tab);
+     
     const tabs = ["all","interview","rejected"];
     for (const t of tabs){
       const tabName = document.getElementById("tab-" + t);
       // console.log(tabName);
       if (t === tab){
+       
          tabName.classList.remove(...tabInactive);
          tabName.classList.add(...tabActive);
       }
@@ -17,4 +22,39 @@
          tabName.classList.add(...tabInactive);
       }
     }
- }
+
+    const pages =[allContainer, interviewContainer, rejectContainer];
+    for (const section of pages ){
+      section.classList.add("hidden");
+    }
+
+
+    if(tab === "all"){
+      // interviewContainer.classList.add("hidden");
+      // rejectContainer.classList.add("hidden"); 
+
+      allContainer.classList.remove("hidden");
+      
+
+
+
+    } else if ( tab === "interview"){
+      // allContainer.classList.add("hidden");
+      // rejectContainer.classList.add("hidden");
+
+      interviewContainer.classList.remove("hidden");
+
+
+
+    } else if (tab === "rejected"){
+      // allContainer.classList.add("hidden");
+      // interviewContainer.classList.add("hidden");
+
+      rejectContainer.classList.remove("hidden");
+    }
+
+
+
+
+ };
+ switchTab(currentTab);
